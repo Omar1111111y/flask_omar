898243d4e7fc37d0cfa44ef4373e2b8ac819c9c6
Omar1111111y <106968928+Omar1111111y@users.noreply.github.com>
2022-07-20 10:22:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 incomes = {'name':'Omar_Style','age': 18}
 
 
-@app.route('/')
+@app.route('/api/omar')
 def get_incomes():
   return jsonify(incomes)
 if __name__ == "__main__":
